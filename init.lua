f953fb0ce9b1ace78a4b98d02bf001e23852e37f
Timo Lausmaa <timo.lausmaa@gmail.com>
2023-03-06 16:04:01 +0200

    Commit config files

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("timo")
+require("timo.packer")
+require("timo.remap")
+
+local set = vim.opt -- set options
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+vim.wo.number = true
+vim.wo.relativenumber = 1
